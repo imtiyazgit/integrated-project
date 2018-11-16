0bd6381795cdc8be2c0a702d0ba63335a2465ad4
Imtiyaz B Moghal <imoghal@virtusa.com>
2018-11-16 14:52:05 -0500

    Mods

---
+++ kubernetes-practice/src/main/java/ch7/ex1/fortuneloop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+trap "exit" SIGINT
+echo Configured to generate new fortune every $INTERVAL seconds mkdir -p /var/htdocs
+while :
+do
+echo "$(date) Writing fortune to /var/htdocs/index.html /usr/games/fortune > /var/htdocs/index.html"
+sleep $INTERVAL
+done
