da2c1e2d06a6a2e124d13f523c06214533bd835b
Imtiyaz B Moghal <imoghal@virtusa.com>
2019-12-20 19:47:48 -0500

    Mods

--- algorithms/src/main/java/algos/linkedlist/nthtolast/NthToLastLinkedList.java
+++ algorithms/src/main/java/algos/linkedlist/nthtolast/NthToLastLinkedList.java
@@ -23,10 +23,16 @@ class Node {
     public void setNext(Node next) {
         this.next = next;
     }
+
+    @Override
+    public String toString() {
+        return value + "";
+    }
 }
 
 class LinkedList {
     Node header;
+    int length;
 
     public void insert(int value) {
         if (header == null) {
@@ -41,7 +47,14 @@ class LinkedList {
             currentNode.setNext(new Node(value));
         }
 
+        length++;
+
     }
+
+    public int size() {
+        return length;
+    }
+
 
     @Override
     public String toString() {
@@ -57,6 +70,52 @@ class LinkedList {
 
 public class NthToLastLinkedList {
 
+    // If you know the length of the linked list, then you can go directly to the node using length - n
+    // You have iterate the list two times. Time Complexity O(2n) = O (n)
+    private static int naiveApproach(LinkedList linkedList, int n) {
+
+        int length = linkedList.length;
+
+        if (n >= length) {
+            return -1;
+        }
+
+        Node currentNode = linkedList.header;
+        for (int i = 1; i < length - n; i++) {
+            currentNode = currentNode.next;
+        }
+
+        return currentNode.value;
+
+    }
+
+
+    // One iteration, Time Complexiy O(n)
+    // Set both currentNode and followerNode to header
+    // Iterate n times only the current node
+    // Then, remaining iterations take both follower node and current node to next until you reach end i.e. current node null
+    private static int nthToLast(LinkedList linkedlist, int n) {
+
+        Node currentNode = linkedlist.header;
+        Node followerNode = linkedlist.header;
+
+        // Set current node after n iterations
+        for (int i = 0; i < n ; i++) {
+            if(currentNode.next == null) {
+                return -1;
+            }
+            currentNode = currentNode.next;
+        }
+
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+            followerNode = followerNode.next;
+        }
+        return followerNode.value;
+
+
+    }
+
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
         linkedList.insert(1);
@@ -66,6 +125,14 @@ public class NthToLastLinkedList {
         linkedList.insert(5);
 
         System.out.println(linkedList);
+
+        //System.out.println(linkedList.size());
+
+        System.out.println(naiveApproach(linkedList, 5));
+
+        //System.out.println(naiveApproach(linkedList, 6));
+
+
 
     }
 }
