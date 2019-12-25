c21bdf203414e00bbf1327fcc056fad8408ded6c
Imtiyaz B Moghal <imoghal@virtusa.com>
2019-12-25 15:15:00 -0500

    Mods

--- algorithms/src/main/java/algos/number/Swap.java
+++ algorithms/src/main/java/algos/number/Swap.java
@@ -5,6 +5,15 @@ public class Swap {
     public static void main(String[] args) {
         int a=1; int b=2;
 
+        // General swap using temp variable
+        int temp = a;
+        a=b;
+        b=temp;
+        System.out.println(a +" and "+b);
+
+        a=1;
+        b=2;
+
         // swap using + -
         a = a+b;
         b = a-b;
