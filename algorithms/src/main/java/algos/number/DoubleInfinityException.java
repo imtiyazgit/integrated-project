e74e8977611864e563bd3640c914b4451c0df071
Imtiyaz B Moghal <imoghal@virtusa.com>
2019-12-25 13:51:11 -0500

    Mods

--- algorithms/src/main/java/algos/number/DoubleInfinityException.java
+++ algorithms/src/main/java/algos/number/DoubleInfinityException.java
@@ -1,12 +1,16 @@
 package algos.number;
 
 public class DoubleInfinityException {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         double d = 1.0/0.0;
 
         Double d2 = 1.0/0.0;
 
         System.out.println(d); // Prints Infinity
         System.out.println(d2); // Prints Infinity
+
+        Thread.sleep(1000);
+        int i = 1/0; // Throws arithmetic exception
+        System.out.println(i);
     }
 }
