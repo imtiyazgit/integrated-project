cd74dff3c27062953724ba6cd28a814f2b599ab9
Imtiyaz Moghal <imtiyazbaig.moghal.nonemployee@pnc.com>
2020-05-26 11:44:42 -0400

    Mods

--- algorithms/src/main/java/algos/linkedlist/reverse/LinkedListReverse.java
+++ algorithms/src/main/java/algos/linkedlist/reverse/LinkedListReverse.java
@@ -46,6 +46,31 @@ public class LinkedListReverse {
             }
         }
 
+        // 1->2->3->4->5->null
+        // 1-> null (prev)
+        // Set head (current pointer)'s next element to the previous element. thats the logic
+        public Node reverse(Node head) {
+            Node prev = null;
+
+            while(head != null) {
+                // Since head's next variable is used to move forward and also we are updating it, store it in local variable
+                Node next = head.next;
+
+                // Set next element to the previous element. Set 1's next to prev null
+                head.next = prev;
+
+                // Once next element is pointed to prev, set prev to the current head
+                prev = head;
+
+                // Move head to the next element using the local variable. do not use head.next since its already pointed backwards
+                head = next;
+            }
+
+            // ALWAYS RETURN PREV.. HEAD IS NULL
+            return prev;
+
+        }
+
         public void printReverse(Node node) {
             if(node == null) {
                 return;
@@ -65,6 +90,9 @@ public class LinkedListReverse {
         linkedList.insert(5);
 
         linkedList.printReverse(linkedList.header);
+
+        Node head = linkedList.reverse(linkedList.header);
+        System.out.println("all done");
 
     }
 }
--- algorithms/src/main/java/algos/linkedlist/reverse/LinkedListReversePT.java
+++ algorithms/src/main/java/algos/linkedlist/reverse/LinkedListReversePT.java
@@ -11,6 +11,8 @@ public class LinkedListReversePT {
         linkedList.insert(new ListNode(5));
 
         linkedList.printReverse(linkedList.head);
+
+        ListNode head = linkedList.reverse(linkedList.head);
         System.out.println("all done");
 
 
@@ -31,6 +33,30 @@ public class LinkedListReversePT {
                 currentNode.next = newNode;
             }
         }
+
+        // 1->2>3>null
+        // null<-1 <- 2 <- 3
+        private static ListNode reverse(ListNode head) {
+            ListNode prev = null;
+
+            while(head != null) {
+                // Since head's next variable is used to move forward and also we are updating it, store it in local variable
+                ListNode next = head.next;
+
+                // Set 1's next to prev null
+                head.next = prev;
+
+                // Once next element is pointed to prev, set prev to the current head
+                prev = head;
+
+                // Move head to the next element using the local variable. do not use head.next since its already pointed backwards
+                head = next;
+            }
+
+            // ALWAYS RETURN PREV.. HEAD IS NULL
+            return prev;
+        }
+
 
         // 1
         // 2
