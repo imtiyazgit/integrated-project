bf62a8adb728b5cb84443312670a99c157732125
Imtiyaz Moghal <imtiyazbaig.moghal.nonemployee@pnc.com>
2020-05-27 12:11:38 -0400

    Mods

--- algorithms/src/main/java/algos/linkedlist/reverse/LinkedListReversePT.java
+++ algorithms/src/main/java/algos/linkedlist/reverse/LinkedListReversePT.java
@@ -10,9 +10,11 @@ public class LinkedListReversePT {
         linkedList.insert(new ListNode(4));
         linkedList.insert(new ListNode(5));
 
-        linkedList.printReverse(linkedList.head);
+        ListNode result = linkedList.reverseSublist(linkedList.head, 2, 4);
 
-        ListNode head = linkedList.reverse(linkedList.head);
+       /* linkedList.printReverse(linkedList.head);
+
+        ListNode head = linkedList.reverse(linkedList.head);*/
         System.out.println("all done");
 
 
@@ -32,6 +34,44 @@ public class LinkedListReversePT {
 
                 currentNode.next = newNode;
             }
+        }
+
+        // 1->2->3->4->5 -> null
+        // 1->2<-3<-4->5->null Reversing next elements between 2,3 and 4
+        // 1->4->3->2->5->null Adjust 1's pointer and 2's pointer
+        private static ListNode reverseSublist(ListNode head, int m, int n) {
+
+            ListNode prev = null;
+            ListNode currentNode = head;
+
+            while(m>1) {
+                prev = currentNode;
+                currentNode = currentNode.next;
+                m--;
+                n--;
+            }
+
+            ListNode connectionNodeToSublist = prev; // which is 1
+            ListNode tailNodeOfSublist = currentNode; // which is 2
+
+            while(n>0) {
+                ListNode next = currentNode.next;
+                currentNode.next = prev;
+                prev = currentNode  ;
+                currentNode = next;
+                n--;
+            }
+
+            // State current node is at 5. Prev is the head of sublist i.e 4.
+            // set connection node to 4 i.e. prev
+            if(connectionNodeToSublist != null) {
+                connectionNodeToSublist.next=prev;
+            }
+
+            // set tail node of sublist to current node i.e. 5
+            tailNodeOfSublist.next = currentNode;
+
+            return head;
         }
 
         // 1->2>3>null
--- algorithms/src/main/java/algos/linkedlist/reverse/sublistreverse/LinkedListSublistReverse.java
+++ algorithms/src/main/java/algos/linkedlist/reverse/sublistreverse/LinkedListSublistReverse.java
@@ -1,6 +1,8 @@
-package algos.linkedlist.reverse;
+package algos.linkedlist.reverse.sublistreverse;
 
-public class LinkedListReverse {
+import algos.linkedlist.reverse.LinkedListReversePT;
+
+public class LinkedListSublistReverse {
     static class Node {
         int data;
         Node next;
@@ -51,32 +53,6 @@ public class LinkedListReverse {
             }
         }
 
-        // 1->2->3->4->5->null
-        // 1-> null (prev)
-        // Set head (current pointer)'s next element to the previous element. thats the logic
-        public Node reverse(Node head) {
-            Node prev = null;
-
-            while(head != null) {
-                // Since head's next variable is used to move forward and also we are updating it, store it in local variable
-                Node next = head.next;
-
-                // Set next element to the previous element. Set 1's next to prev null
-                head.next = prev;
-
-                // Once next element is pointed to prev, set prev to the current head
-                prev = head;
-
-                // Move head to the next element using the local variable. do not use head.next since its already pointed backwards
-                head = next;
-            }
-
-            // ALWAYS RETURN PREV.. HEAD IS NULL
-            return prev;
-
-        }
-
-
         // 1->2->3>4->5->null
         // 1->4->3>2->5 -> null
         // start is 2 and end is 4
@@ -109,14 +85,6 @@ public class LinkedListReverse {
             return head;
         }
 
-        public void printReverse(Node node) {
-            if(node == null) {
-                return;
-            }
-
-            printReverse(node.next);
-            System.out.println(node.data);
-        }
     }
 
     public static void main(String[] args) {
@@ -127,12 +95,9 @@ public class LinkedListReverse {
         linkedList.insert(4);
         linkedList.insert(5);
 
-        //linkedList.printReverse(linkedList.header);
+        Node result = linkedList.reverseSublist(linkedList.header, 2, 4);
 
-        //Node head = linkedList.reverse(linkedList.header);
-
-        Node node = linkedList.reverseSublist(linkedList.header, 2,4);
-        System.out.println("all done");
+        System.out.println("all done"); // 1->4->3>2>5->null
 
     }
 }
